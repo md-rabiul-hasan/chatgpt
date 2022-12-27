7c6b9907d3b53dde74a208d6b7184046b9bc2386
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-12-27 12:36:12 +0600

    api error solve

--- client/main.js
+++ client/main.js
@@ -6,13 +6,13 @@ const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
-function loader(element){
+function loader(element) {
   element.textContent = "";
 
   loadInterval = setInterval(() => {
     element.textContent += ".";
 
-    if(element.textContent == "...."){
+    if (element.textContent == "....") {
       element.textContent = "";
     }
 
@@ -20,36 +20,36 @@ function loader(element){
 }
 
 
-function typeText(element, text){
+function typeText(element, text) {
   let index = 0;
 
   let interval = setInterval(() => {
-    if(index < text.length){
+    if (index < text.length) {
       element.innerHTML += text.charAt(index);
       index++;
-    }else{
+    } else {
       clearInterval(interval);
     }
   }, 20);
 }
 
 
-function generateUniqueId(){
-  const timestamp         = Date.now();
-  const randomNumber      = Math.random();
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
   const hexaDecimalString = randomNumber.toString(16);
-  
+
   return `id-${timestamp}-${hexaDecimalString}`;
 }
 
 
-function chatStripe(isAi, value, uniqueId){
+function chatStripe(isAi, value, uniqueId) {
   return (
     `
       <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
           <div class="profile">
-            <img src="${isAi ? bot: user}" alt="${isAi ? 'bot' : 'user'}" />
+            <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}" />
           </div>
           <div class="message" id=${uniqueId}>${value}</div>
         </div>
@@ -75,44 +75,40 @@ const handleSubmit = async (e) => {
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
   const messageDiv = document.getElementById(uniqueId);
-  
+
   loader(messageDiv);
 
-  // fetch data from server = bot's response
-
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('http://localhost:5000/', {
     method: 'POST',
-    headers:{
-      'Content-Type': 'application/json'
+    headers: {
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       prompt: data.get('prompt')
     })
   })
 
-  clearInterval(loadInterval);
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = " "
 
-  messageDiv.innerHTML= "";
-
-  if(response.ok){
+  if (response.ok) {
     const data = await response.json();
-    const parseData = data.bot.trim();
+    const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
-    typeText(messageDiv, parseData);
-  }else{
-    const err = await response.text();
-    messageDiv.innerHTML = "Something went wrong";
-    alert(err);
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
+
+    messageDiv.innerHTML = "Something went wrong"
+    alert(err)
   }
-
-
 
 
 }
 
 form.addEventListener("submit", handleSubmit);
 form.addEventListener('keyup', (e) => {
-  if(e.keyCode === 13){ // enter key
+  if (e.keyCode === 13) { // enter key
     handleSubmit(e);
   }
 })
